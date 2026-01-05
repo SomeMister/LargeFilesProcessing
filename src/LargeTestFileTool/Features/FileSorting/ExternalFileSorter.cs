64a27747e350ca9747cf9eaa5c7301fd652b2238
Dmitry <dima.dmitry33@gmail.com>
2026-01-05 13:36:43 +0100

    Clean up

--- src/LargeTestFileTool/Features/FileSorting/ExternalFileSorter.cs
+++ src/LargeTestFileTool/Features/FileSorting/ExternalFileSorter.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
-using System.IO;
 using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace LargeTestFileTool.Features.FileSorting;
 
